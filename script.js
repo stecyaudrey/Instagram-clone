58dc75a435b7535793e0606b0a3cb2ef9f1fdfb4
Stecy Audrey <stecy3204@gmail.com>
2026-03-31 02:29:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const followBtn =
+document.getElementById("follow-btn");
+followBtn.addEventListener("click", function() {this.classList.toggle("following");
+  if(this.innerText === "Follow"){innerText === "Following";} else {
+    this.innerText === "Follow";
+  }
+});
